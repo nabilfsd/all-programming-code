7e048e56ec5ef6eeb4b461cd7ebbfd222b01c90b
nabilfsd <nabil.the.fsd@gmail.com>
2021-12-23 22:29:37 +0600

    python: check two number are equal or not

---
+++ Python/BitBox/Lecture2/check_equal_number.py
@@ -0,0 +1,9 @@
+# check two number are equal or not.
+
+number1 = int(input("Enter Number1: "))
+number2 = int(input("Enter Number2: "))
+
+if (number1 == number2):
+    print("Yes, %d is equal to %d" % (number1, number2))
+else:
+    print("No, %d is not match with %d" % (number1, number2))
